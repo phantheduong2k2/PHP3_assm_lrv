1a65de57a7fd041ce406285f4cfcd0f5edda7313
your name <duongptph14649@fpt.edu.vn>
2022-08-12 08:26:32 +0700

    Procfile for Heroku

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('avatar');
+            $table->string('avatar')->nullable();
             $table->tinyInteger('level')->nullable()->default(0);
 
             $table->timestamp('email_verified_at')->nullable();
